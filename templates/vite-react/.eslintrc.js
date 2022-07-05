c754ba16412af27d81581e27a43bd0412771b677
kiver <kiver@omegasistemas.net.br>
2022-07-05 17:41:44 -0400

    feat: Início do projeto do CLI

---
+++ templates/vite-react/.eslintrc.js
@@ -0,0 +1,64 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es6: true,
+        node: true,
+        browser: true,
+        amd: true,
+        jest: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:jsx-a11y/recommended",
+        "plugin:prettier/recommended", // Make sure this is always the last element in the array.
+    ],
+
+    settings: {
+        react: {
+            version: "detect",
+            pragma: "React", // Pragma to use, default to "React"
+            // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+        },
+        "import/resolver": {
+            node: {
+                paths: ["src"],
+                extensions: [".js", ".jsx"],
+            },
+        },
+    },
+    plugins: ["react", "simple-import-sort", "prettier"],
+    parser: "@babel/eslint-parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+            experimentalObjectRestSpread: true,
+        },
+        babelOptions: {
+            presets: ["@babel/preset-react"],
+        },
+        sourceType: "module",
+        requireConfigFile: false,
+    },
+    rules: {
+        "react/react-in-jsx-scope": "off",
+        "array-bracket-spacing": [1, "never"],
+        "block-scoped-var": 0,
+        "react/jsx-filename-extension": ["warn", { extensions: [".js", ".jsx"] }],
+        "import/prefer-default-export": "off",
+        "prettier/prettier": ["error", {}, { usePrettierrc: true }],
+        "jsx-a11y/accessible-emoji": "off",
+        "react/prop-types": "off",
+        "simple-import-sort/imports": "error",
+        "simple-import-sort/exports": "error",
+        "jsx-a11y/anchor-is-valid": [
+            "error",
+            {
+                components: ["Link"],
+                specialLink: ["hrefLeft", "hrefRight"],
+                aspects: ["invalidHref", "preferButton"],
+            },
+        ],
+    },
+};
